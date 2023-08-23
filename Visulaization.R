35bf0a352b3f52201482f584b8754e866f23d258
sls-chundru <sls.chundru@gmail.com>
2023-08-22 22:32:23 -0500

    Fixed code for visulaization

--- Visulaization.R
+++ Visulaization.R
@@ -1,4 +1,3 @@
-set.seed(1919)                                 # Create example data
-x1 <- rnorm(1000)
-y1 <- x1 + rnorm(1000)
-plot(x1, y1)                                   # Apply plot function
+
+message <-"Hello World!"
+print(message)
